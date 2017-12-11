0cdd308ce6a3e872bc9b556fd7cd7adb0ad7f8f0
hirnips <hdave3180@gmail.com>
2017-12-11 10:39:57 -0500

    Applicant_Profiles Table Created

---
+++ CareerCloud.Pocos/ApplicantProfilesPoco.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Applicant_Profiles")]
+    public class ApplicantProfilesPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Login { get; set; }
+
+        [Column("Current_Salary")]
+        public Decimal? CurrentSalary { get; set; }
+
+        [Column("Current_Rate")]
+        public Decimal? CurrentRate { get; set; }
+
+        public string Currency { get; set; }
+
+        [Column("Country_Code")]
+        public string CountryCode { get; set; }
+
+        [Column("State_Province_Code")]
+        public string StateProvinceCode { get; set; }
+
+        [Column("Street_Address")]
+        public int StreetAddress { get; set; }
+
+        [Column("City_Town")]
+        public string CityTown { get; set; }
+
+        [Column("Zip_Postal_Code")]
+        public string ZipPostalCode { get; set; }
+
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
+
+    }
+}
