12da0a82ee0c62e4a809b072d7571a1a6e7879ef
hirnips <hdave3180@gmail.com>
2017-12-11 22:26:29 -0500

    Company_Locations Table Created

---
+++ CareerCloud.Pocos/CompanyLocationsPoco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Company_Locations")]
+    public class CompanyLocationsPoco: IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Company { get; set; }
+
+        [Column("Country_Code")]
+        public string CountryCode { get; set; }
+
+        [Column("State_Province_Code")]
+        public string StateProvinceCode { get; set; }
+
+        [Column("Street_Address")]
+        public string StreetAddress { get; set; }
+
+        [Column("City_Town")]
+        public string CityTown { get; set; }
+
+        [Column("Zip_Postal_Code")]
+        public string ZipPostalCode { get; set; }
+
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
+
+    }
+}
