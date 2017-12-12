3ca93da71f8e508a06a112c4bcad7dfe65d58efe
hirnips <hdave3180@gmail.com>
2017-12-11 22:58:00 -0500

    System_Country_Codes Table Created

---
+++ CareerCloud.Pocos/SystemCountryCodesPoco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("System_Country_Codes")]
+    public class SystemCountryCodesPoco
+    {
+        [Key]
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
