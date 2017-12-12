99a65c0076a694f602648a066e4c450186f306e3
hirnips <hdave3180@gmail.com>
2017-12-11 22:55:25 -0500

    Security_Roles table Created

---
+++ CareerCloud.Pocos/SecurityRolesPoco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Security_Roles")]
+    public class SecurityRolesPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Role { get; set; }
+
+        [Column("Is_Inactive")]
+        public Boolean IsInactive { get; set; }
+    }
+}
