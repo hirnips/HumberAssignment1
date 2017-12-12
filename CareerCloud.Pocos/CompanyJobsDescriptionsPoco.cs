6c94752d715920d66806f56a10d4edd1249f0912
hirnips <hdave3180@gmail.com>
2017-12-11 22:20:30 -0500

    Company_Jobs_Descriptions table created

---
+++ CareerCloud.Pocos/CompanyJobsDescriptionsPoco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Company_Jobs_Descriptions")]
+    public class CompanyJobsDescriptionsPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Job { get; set; }
+
+        [Column("Job_Name")]
+        public string JobName { get; set; }
+
+        [Column("Job_Descriptions")]
+        public string JobDescriptions { get; set; }
+
+        [Column("Time_Stamp")]
+        public Byte[] Time_Stamp { get; set; }
+    }
+}
