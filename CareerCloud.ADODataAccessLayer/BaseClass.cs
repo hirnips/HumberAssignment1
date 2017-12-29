1b83bdfdf192fc575747b863490a9bd092047a92
hirnips <hdave3180@gmail.com>
2017-12-29 17:25:12 -0500

    ApplicantEducationRepository added with all methods

---
+++ CareerCloud.ADODataAccessLayer/BaseClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class BaseClass
+    {
+        protected string _connString;
+
+        public BaseClass()
+        {
+            _connString = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+        }
+    }
+}
