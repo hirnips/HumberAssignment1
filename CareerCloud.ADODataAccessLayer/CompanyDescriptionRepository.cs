dbc9d04c2118d51207bc9b24228166fc797d9556
hirnips <hdave3180@gmail.com>
2017-12-30 18:20:09 -0500

    CompanyDescriptionRepository class done with all method

--- CareerCloud.ADODataAccessLayer/CompanyDescriptionRepository.cs
+++ CareerCloud.ADODataAccessLayer/CompanyDescriptionRepository.cs
@@ -16,7 +16,37 @@ namespace CareerCloud.ADODataAccessLayer
     {
         public void Add(params CompanyDescriptionPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                foreach (CompanyDescriptionPoco poco in items)
+                {
+                    cmd.CommandText = @"INSERT INTO [dbo].[Company_Descriptions]
+                                                   ([Id]
+                                                   ,[Company]
+                                                   ,[LanguageID]
+                                                   ,[Company_Name]
+                                                   ,[Company_Description])
+                                             VALUES
+                                                   (@Id
+                                                   ,@Company
+                                                   ,@LanguageID
+                                                   ,@Company_Name
+                                                   ,@Company_Description)";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Company", poco.Company);
+                    cmd.Parameters.AddWithValue("@LanguageID", poco.LanguageId);
+                    cmd.Parameters.AddWithValue("@Company_Name", poco.CompanyName);
+                    cmd.Parameters.AddWithValue("@Company_Description", poco.CompanyDescription);
+                   
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -26,7 +56,43 @@ namespace CareerCloud.ADODataAccessLayer
 
         public IList<CompanyDescriptionPoco> GetAll(params Expression<Func<CompanyDescriptionPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                cmd.CommandText = @"SELECT [Id]
+                                          ,[Company]
+                                          ,[LanguageID]
+                                          ,[Company_Name]
+                                          ,[Company_Description]
+                                          ,[Time_Stamp]
+                                      FROM [dbo].[Company_Descriptions]";
+
+                int counter = 0;
+                CompanyDescriptionPoco[] pocos = new CompanyDescriptionPoco[601];
+
+                conn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+               
+                while (rdr.Read())
+                {
+                    CompanyDescriptionPoco poco = new CompanyDescriptionPoco();
+
+                    poco.Id = rdr.GetGuid(0);
+                    poco.Company  = rdr.GetGuid(1);
+                    poco.LanguageId = rdr.GetString(2);
+                    poco.CompanyName = rdr.GetString(3);
+                    poco.CompanyDescription  = rdr.GetString(4);
+                    poco.TimeStamp = (byte[])rdr[5];
+
+                    pocos[counter] = poco;
+                    counter++;
+                }
+                conn.Close();
+
+                return pocos.Where(a => a != null).ToList();
+            }
         }
 
         public IList<CompanyDescriptionPoco> GetList(Func<CompanyDescriptionPoco, bool> where, params Expression<Func<CompanyDescriptionPoco, object>>[] navigationProperties)
@@ -36,17 +102,57 @@ namespace CareerCloud.ADODataAccessLayer
 
         public CompanyDescriptionPoco GetSingle(Func<CompanyDescriptionPoco, bool> where, params Expression<Func<CompanyDescriptionPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            CompanyDescriptionPoco[] pocos = GetAll().ToArray();
+            return pocos.Where(where).ToList().FirstOrDefault();
         }
 
         public void Remove(params CompanyDescriptionPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                foreach (CompanyDescriptionPoco poco in items)
+                {
+                    cmd.CommandText = @"DELETE FROM [dbo].[Company_Descriptions]
+                                        WHERE Id=@Id";
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
         }
 
         public void Update(params CompanyDescriptionPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                foreach (CompanyDescriptionPoco poco in items)
+                {
+                    cmd.CommandText = @"UPDATE [dbo].[Company_Descriptions]
+                                        SET  [Company] = @Company
+                                            ,[LanguageID] = @LanguageID
+                                            ,[Company_Name] = @Company_Name
+                                            ,[Company_Description] = @Company_Description
+                                        WHERE Id = @Id";
+
+                    cmd.Parameters.AddWithValue("@Id", poco.Id);
+                    cmd.Parameters.AddWithValue("@Company", poco.Company);
+                    cmd.Parameters.AddWithValue("@LanguageID", poco.LanguageId);
+                    cmd.Parameters.AddWithValue("@Company_Name", poco.CompanyName);
+                    cmd.Parameters.AddWithValue("@Company_Description", poco.CompanyDescription);
+                   
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
         }
     }
 }
