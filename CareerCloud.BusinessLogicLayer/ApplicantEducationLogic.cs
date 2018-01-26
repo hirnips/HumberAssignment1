39d4783050b0cbdf4346c565bf1e565801da8069
hirnips <hdave3180@gmail.com>
2018-01-26 15:49:33 -0500

    Application Eduaction Logic Created

---
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -0,0 +1,64 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantEducationLogic : BaseLogic<ApplicantEducationPoco>
+    {
+        public ApplicantEducationLogic(IDataRepository<ApplicantEducationPoco> repository) : base(repository)
+        {
+        }
+        public override void Add(ApplicantEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantEducationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(ApplicantEducationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            string[] requiredExtendedPasswordChars = new string[] { "$", "*", "#", "_", "@" };
+
+            foreach (var poco in pocos)
+            {
+                //Check major is null or not and length is more than 3
+                if (string.IsNullOrEmpty(poco.Major))
+                {
+                    exceptions.Add(new ValidationException(107, $"Cannot be empty or less than 3 characters {poco.Id}."));
+                }
+                else if (poco.Major.Length < 3)
+                {
+                    exceptions.Add(new ValidationException(107, $"Cannot be empty or less than 3 characters { poco.Id}."));
+                }
+                
+                //Check start date should not be greater than today
+                if(poco.StartDate > DateTime.Today)
+                {
+                    exceptions.Add(new ValidationException(108, $"Cannot be greater than today { poco.Id }."));
+                }
+
+                //Check CompletionDate cannot be earlier than StartDate
+                if(poco.CompletionDate < poco.StartDate)
+                {
+                    exceptions.Add(new ValidationException(109, $"CompletionDate cannot be earlier than StartDate { poco.Id }."));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
