b1d694d6d257a9a9a2869953801f4dc2170a9bed
Hiren Dave <hdave3180@outlook.com>
2018-04-07 00:56:09 -0400

    CareerCloud.WCF Security Logic Done

--- CareerCloud.WCF/ISecurity.cs
+++ CareerCloud.WCF/ISecurity.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using CareerCloud.Pocos;
 
 namespace CareerCloud.WCF
 {
@@ -12,6 +13,73 @@ namespace CareerCloud.WCF
     public interface ISecurity
     {
         [OperationContract]
-        void DoWork();
+        void SecurityLogin(SecurityLoginPoco[] items);
+
+        [OperationContract]
+        List<SecurityLoginPoco> GetAllSecurityLogin();
+
+        [OperationContract]
+        SecurityLoginPoco GetSingleSecurityLogin(string Id);
+
+        [OperationContract]
+        void RemoveSecurityLogin(SecurityLoginPoco[] items);
+
+        [OperationContract]
+        void UpdateSecurityLogin(SecurityLoginPoco[] items);
+
+
+
+
+        [OperationContract]
+        void SecurityLoginsLog(SecurityLoginsLogPoco[] items);
+
+        [OperationContract]
+        List<SecurityLoginsLogPoco> GetAllSecurityLoginsLog();
+
+        [OperationContract]
+        SecurityLoginsLogPoco GetSingleSecurityLoginsLog(string Id);
+
+        [OperationContract]
+        void RemoveSecurityLoginsLog(SecurityLoginsLogPoco[] items);
+
+        [OperationContract]
+        void UpdateSecurityLoginsLog(SecurityLoginsLogPoco[] items);
+
+
+
+
+        [OperationContract]
+        void SecurityLoginsRole(SecurityLoginsRolePoco[] items);
+
+        [OperationContract]
+        List<SecurityLoginsRolePoco> GetAllSecurityLoginsRole();
+
+        [OperationContract]
+        SecurityLoginsRolePoco GetSingleSecurityLoginsRole(string Id);
+
+        [OperationContract]
+        void RemoveSecurityLoginsRole(SecurityLoginsRolePoco[] items);
+
+        [OperationContract]
+        void UpdateSecurityLoginsRole(SecurityLoginsRolePoco[] items);
+
+
+
+
+
+        [OperationContract]
+        void SecurityRole(SecurityRolePoco[] items);
+
+        [OperationContract]
+        List<SecurityRolePoco> GetAllSecurityRole();
+
+        [OperationContract]
+        SecurityRolePoco GetSingleSecurityRole(string Id);
+
+        [OperationContract]
+        void RemoveSecurityRole(SecurityRolePoco[] items);
+
+        [OperationContract]
+        void UpdateSecurityRole(SecurityRolePoco[] items);
     }
 }
--- CareerCloud.WCF/Security.cs
+++ CareerCloud.WCF/Security.cs
@@ -4,14 +4,111 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using CareerCloud.Pocos;
 
 namespace CareerCloud.WCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Security" in both code and config file together.
     public class Security : ISecurity
     {
-        public void DoWork()
+        public List<SecurityLoginPoco> GetAllSecurityLogin()
         {
+            throw new NotImplementedException();
+        }
+
+        public List<SecurityLoginsLogPoco> GetAllSecurityLoginsLog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SecurityLoginsRolePoco> GetAllSecurityLoginsRole()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SecurityRolePoco> GetAllSecurityRole()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityLoginPoco GetSingleSecurityLogin(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityLoginsLogPoco GetSingleSecurityLoginsLog(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityLoginsRolePoco GetSingleSecurityLoginsRole(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityRolePoco GetSingleSecurityRole(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSecurityLogin(SecurityLoginPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSecurityLoginsLog(SecurityLoginsLogPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSecurityLoginsRole(SecurityLoginsRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSecurityRole(SecurityRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SecurityLogin(SecurityLoginPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SecurityLoginsLog(SecurityLoginsLogPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SecurityLoginsRole(SecurityLoginsRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SecurityRole(SecurityRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSecurityLogin(SecurityLoginPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSecurityLoginsLog(SecurityLoginsLogPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSecurityLoginsRole(SecurityLoginsRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSecurityRole(SecurityRolePoco[] items)
+        {
+            throw new NotImplementedException();
         }
     }
 }
