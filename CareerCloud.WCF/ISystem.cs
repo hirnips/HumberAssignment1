655c087e937930a2a01a8c4c2f6795bc8d35bb0d
Hiren Dave <hdave3180@outlook.com>
2018-04-07 01:00:33 -0400

    CareerCloud.WCF system service contract done

--- CareerCloud.WCF/ISystem.cs
+++ CareerCloud.WCF/ISystem.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using CareerCloud.Pocos;
 
 namespace CareerCloud.WCF
 {
@@ -12,6 +13,36 @@ namespace CareerCloud.WCF
     public interface ISystem
     {
         [OperationContract]
-        void DoWork();
+        void SystemCountryCode(SystemCountryCodePoco[] items);
+
+        [OperationContract]
+        List<SystemCountryCodePoco> GetAllSystemCountryCode();
+
+        [OperationContract]
+        SystemCountryCodePoco GetSingleSystemCountryCode(string Id);
+
+        [OperationContract]
+        void RemoveSystemCountryCode(SystemCountryCodePoco[] items);
+
+        [OperationContract]
+        void UpdateSystemCountryCode(SystemCountryCodePoco[] items);
+
+
+
+
+        [OperationContract]
+        void SystemLanguageCode(SystemLanguageCodePoco[] items);
+
+        [OperationContract]
+        List<SystemLanguageCodePoco> GetAllSystemLanguageCode();
+
+        [OperationContract]
+        SystemLanguageCodePoco GetSingleSystemLanguageCode(string Id);
+
+        [OperationContract]
+        void RemoveSystemLanguageCode(SystemLanguageCodePoco[] items);
+
+        [OperationContract]
+        void UpdateSystemLanguageCode(SystemLanguageCodePoco[] items);
     }
 }
--- CareerCloud.WCF/System.cs
+++ CareerCloud.WCF/System.cs
@@ -4,14 +4,61 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using CareerCloud.Pocos;
 
 namespace CareerCloud.WCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "System" in both code and config file together.
     public class System : ISystem
     {
-        public void DoWork()
+        public List<SystemCountryCodePoco> GetAllSystemCountryCode()
         {
+            throw new NotImplementedException();
+        }
+
+        public List<SystemLanguageCodePoco> GetAllSystemLanguageCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemCountryCodePoco GetSingleSystemCountryCode(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SystemLanguageCodePoco GetSingleSystemLanguageCode(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSystemCountryCode(SystemCountryCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSystemLanguageCode(SystemLanguageCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SystemCountryCode(SystemCountryCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SystemLanguageCode(SystemLanguageCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSystemCountryCode(SystemCountryCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSystemLanguageCode(SystemLanguageCodePoco[] items)
+        {
+            throw new NotImplementedException();
         }
     }
 }
