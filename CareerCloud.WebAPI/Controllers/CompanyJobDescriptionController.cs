e51752f637bb82a9b38d987b4f1e81ed70399ae5
Hiren Dave <hdave3180@outlook.com>
2018-04-18 18:37:47 -0400

    Developed All Controller and Add RoutePrefix on them

---
+++ CareerCloud.WebAPI/Controllers/CompanyJobDescriptionController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [RoutePrefix("api/careercloud/company/v1")]
+    public class CompanyJobDescriptionController : ApiController
+    {
+    }
+}
---
+++ CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [RoutePrefix("api/careercloud/system/v1")]
+    public class SystemCountryCodeController : ApiController
+    {
+    }
+}
