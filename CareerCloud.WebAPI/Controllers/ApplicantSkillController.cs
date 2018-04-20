1a41afea39dd07eaa9094a5a824770c7d66bdb3f
Hiren Dave <hdave3180@outlook.com>
2018-04-20 19:23:09 -0400

    ApplicantSkillController done

--- CareerCloud.WebAPI/Controllers/ApplicantSkillController.cs
+++ CareerCloud.WebAPI/Controllers/ApplicantSkillController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,70 @@ namespace CareerCloud.WebAPI.Controllers
         public ApplicantSkillController()
         {
             _logic = new ApplicantSkillLogic(new EFGenericRepository<ApplicantSkillPoco>(false));
+        }
+
+        [HttpGet]
+        [Route("skill/{applicantSkillId}")]
+        [ResponseType(typeof(ApplicantSkillPoco))]
+        public IHttpActionResult GetApplicantSkill(Guid applicantSkillId)
+        {
+            ApplicantSkillPoco applicantSkill = _logic.Get(applicantSkillId);
+            if (applicantSkill == null)
+            {
+                return NotFound();
+            }
+            return Ok(applicantSkill);
+        }
+
+
+        [HttpGet]
+        [Route("skill")]
+        [ResponseType(typeof(List<ApplicantSkillPoco>))]
+        public IHttpActionResult GetAllApplicantSkill()
+        {
+            List<ApplicantSkillPoco> applicantSkill = _logic.GetAll();
+            if (applicantSkill == null)
+            {
+                return NotFound();
+            }
+            return Ok(applicantSkill);
+        }
+
+        [HttpPost]
+        [Route("skill")]
+        public IHttpActionResult SaveApplicantSkill(ApplicantSkillPoco[] applicantSkill)
+        {
+            if (applicantSkill == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(applicantSkill);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("skill")]
+        public IHttpActionResult UpdateApplicantSkill(ApplicantSkillPoco[] applicantSkill)
+        {
+            if (applicantSkill == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(applicantSkill);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("skill")]
+        public IHttpActionResult DeleteApplicantSkill(ApplicantSkillPoco[] applicantSkill)
+        {
+            if (applicantSkill == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(applicantSkill);
+            return Ok();
         }
     }
 }
