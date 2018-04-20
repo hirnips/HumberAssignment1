4885eb615ebd63752ea3a88bccf0830b7fe26bb5
Hiren Dave <hdave3180@outlook.com>
2018-04-20 19:41:05 -0400

    CompanyJobDescriptionController done

--- CareerCloud.WebAPI/Controllers/CompanyJobDescriptionController.cs
+++ CareerCloud.WebAPI/Controllers/CompanyJobDescriptionController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,70 @@ namespace CareerCloud.WebAPI.Controllers
         public CompanyJobDescriptionController()
         {
             _logic = new CompanyJobDescriptionLogic(new EFGenericRepository<CompanyJobDescriptionPoco>(false));
+        }
+
+        [HttpGet]
+        [Route("jobDescription/{companyJobDescriptionId}")]
+        [ResponseType(typeof(CompanyJobDescriptionPoco))]
+        public IHttpActionResult GetCompanyJobDescription(Guid companyJobDescriptionId)
+        {
+            CompanyJobDescriptionPoco companyJobDescription = _logic.Get(companyJobDescriptionId);
+            if (companyJobDescription == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyJobDescription);
+        }
+
+
+        [HttpGet]
+        [Route("jobDescription")]
+        [ResponseType(typeof(List<CompanyJobDescriptionPoco>))]
+        public IHttpActionResult GetAllCompanyJobDescription()
+        {
+            List<CompanyJobDescriptionPoco> companyJobDescription = _logic.GetAll();
+            if (companyJobDescription == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyJobDescription);
+        }
+
+        [HttpPost]
+        [Route("jobDescription")]
+        public IHttpActionResult SaveCompanyJobDescription(CompanyJobDescriptionPoco[] companyJobDescription)
+        {
+            if (companyJobDescription == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(companyJobDescription);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("jobDescription")]
+        public IHttpActionResult UpdateCompanyJobDescription(CompanyJobDescriptionPoco[] companyJobDescription)
+        {
+            if (companyJobDescription == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(companyJobDescription);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("jobDescription")]
+        public IHttpActionResult DeleteCompanyJobDescription(CompanyJobDescriptionPoco[] companyJobDescription)
+        {
+            if (companyJobDescription == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(companyJobDescription);
+            return Ok();
         }
     }
 }
