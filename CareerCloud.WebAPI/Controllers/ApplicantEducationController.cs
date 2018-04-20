069ce1cfc6b3d3ac9227a86efea61f1222bb9226
Hiren Dave <hdave3180@outlook.com>
2018-04-20 18:33:27 -0400

    Apply CURD operation on ApplicantEducation Controller

--- CareerCloud.WebAPI/Controllers/ApplicantEducationController.cs
+++ CareerCloud.WebAPI/Controllers/ApplicantEducationController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,70 @@ namespace CareerCloud.WebAPI.Controllers
         public ApplicantEducationController()
         {
             _logic = new ApplicantEducationLogic(new EFGenericRepository<ApplicantEducationPoco>(false));
+        }
+
+        [HttpGet]
+        [Route("education/{applicantEducationId}")]
+        [ResponseType(typeof(ApplicantEducationPoco))]
+        public IHttpActionResult GetApplicantEducation(Guid applicantEducationId)
+        {
+            ApplicantEducationPoco applicantEducation = _logic.Get(applicantEducationId);
+            if(applicantEducation == null)
+            {
+                return NotFound();
+            }
+            return Ok(applicantEducation);
+        }
+
+
+        [HttpGet]
+        [Route("education")]
+        [ResponseType(typeof(List<ApplicantEducationPoco>))]
+        public IHttpActionResult GetAllApplicantEducation(Guid applicantEducationId)
+        {
+            List<ApplicantEducationPoco> applicantEducations = _logic.GetAll();
+            if (applicantEducations == null)
+            {
+                return NotFound();
+            }
+            return Ok(applicantEducations);
+        }
+
+        [HttpPost]
+        [Route("education")]
+        public IHttpActionResult SaveApplicantEducation(ApplicantEducationPoco[] applicantEducations)
+        {
+            if(applicantEducations == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(applicantEducations);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("education")]
+        public IHttpActionResult UpdateApplicantEducation(ApplicantEducationPoco[] applicantEducations)
+        {
+            if (applicantEducations == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(applicantEducations);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("education")]
+        public IHttpActionResult DeleteApplicantEducation(ApplicantEducationPoco[] applicantEducations)
+        {
+            if (applicantEducations == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(applicantEducations);
+            return Ok();
         }
     }
 }
