f139f74f30856ffb1f433cc9d3bef199ff23d2ae
Hiren Dave <hdave3180@outlook.com>
2018-04-20 19:18:01 -0400

    ApplicantResumeController Done

--- CareerCloud.WebAPI/Controllers/ApplicantResumeController.cs
+++ CareerCloud.WebAPI/Controllers/ApplicantResumeController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,71 @@ namespace CareerCloud.WebAPI.Controllers
         public ApplicantResumeController()
         {
             _logic = new ApplicantResumeLogic(new EFGenericRepository<ApplicantResumePoco>(false));
+        }
+
+
+        [HttpGet]
+        [Route("resume/{applicantResumeId}")]
+        [ResponseType(typeof(ApplicantResumePoco))]
+        public IHttpActionResult GetApplicantResume(Guid applicantResumeId)
+        {
+            ApplicantResumePoco applicantResume = _logic.Get(applicantResumeId);
+            if (applicantResume == null)
+            {
+                return NotFound();
+            }
+            return Ok(applicantResume);
+        }
+
+
+        [HttpGet]
+        [Route("resume")]
+        [ResponseType(typeof(List<ApplicantResumePoco>))]
+        public IHttpActionResult GetAllApplicantResume()
+        {
+            List<ApplicantResumePoco> applicantResume = _logic.GetAll();
+            if (applicantResume == null)
+            {
+                return NotFound();
+            }
+            return Ok(applicantResume);
+        }
+
+        [HttpPost]
+        [Route("resume")]
+        public IHttpActionResult SaveApplicantResume(ApplicantResumePoco[] applicantResume)
+        {
+            if (applicantResume == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(applicantResume);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("resume")]
+        public IHttpActionResult UpdateApplicantResume(ApplicantResumePoco[] applicantResume)
+        {
+            if (applicantResume == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(applicantResume);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("resume")]
+        public IHttpActionResult DeleteApplicantResume(ApplicantResumePoco[] applicantResume)
+        {
+            if (applicantResume == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(applicantResume);
+            return Ok();
         }
     }
 }
