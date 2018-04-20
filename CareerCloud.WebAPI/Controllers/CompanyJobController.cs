4a1e9ffb865e9353d2dd3612b16a949ecc2b3530
Hiren Dave <hdave3180@outlook.com>
2018-04-20 19:35:11 -0400

    CompanyJobController Done

--- CareerCloud.WebAPI/Controllers/CompanyJobController.cs
+++ CareerCloud.WebAPI/Controllers/CompanyJobController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,70 @@ namespace CareerCloud.WebAPI.Controllers
         public CompanyJobController()
         {
             _logic = new CompanyJobLogic(new EFGenericRepository<CompanyJobPoco>(false));
+        }
+
+        [HttpGet]
+        [Route("companyJob/{companyJobId}")]
+        [ResponseType(typeof(CompanyJobPoco))]
+        public IHttpActionResult GetCompanyJob(Guid companyJobId)
+        {
+            CompanyJobPoco companyJob = _logic.Get(companyJobId);
+            if (companyJob == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyJob);
+        }
+
+
+        [HttpGet]
+        [Route("companyJob")]
+        [ResponseType(typeof(List<CompanyJobPoco>))]
+        public IHttpActionResult GetAllCompanyJob()
+        {
+            List<CompanyJobPoco> companyJob = _logic.GetAll();
+            if (companyJob == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyJob);
+        }
+
+        [HttpPost]
+        [Route("companyJob")]
+        public IHttpActionResult SaveCompanyJob(CompanyJobPoco[] companyJob)
+        {
+            if (companyJob == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(companyJob);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("companyJob")]
+        public IHttpActionResult UpdateCompanyJob(CompanyJobPoco[] companyJob)
+        {
+            if (companyJob == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(companyJob);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("companyJob")]
+        public IHttpActionResult DeleteCompanyJob(CompanyJobPoco[] companyJob)
+        {
+            if (companyJob == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(companyJob);
+            return Ok();
         }
     }
 }
