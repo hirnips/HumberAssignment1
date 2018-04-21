89b6a13355565cd7e62fdda88b3fed2cd303ae21
Hiren Dave <hdave3180@outlook.com>
2018-04-20 20:52:51 -0400

    SecurityLoginsLogController Done

--- CareerCloud.WebAPI/Controllers/SecurityLoginsLogController.cs
+++ CareerCloud.WebAPI/Controllers/SecurityLoginsLogController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,70 @@ namespace CareerCloud.WebAPI.Controllers
         public SecurityLoginsLogController()
         {
             _logic = new SecurityLoginsLogLogic(new EFGenericRepository<SecurityLoginsLogPoco>(false));
+        }
+
+        [HttpGet]
+        [Route("loginLog/{securityLoginLogId}")]
+        [ResponseType(typeof(SecurityLoginsLogPoco))]
+        public IHttpActionResult GetSecurityLoginLog(Guid securityLoginLogId)
+        {
+            SecurityLoginsLogPoco securityLoginLog = _logic.Get(securityLoginLogId);
+            if (securityLoginLog == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLoginLog);
+        }
+
+
+        [HttpGet]
+        [Route("loginLog")]
+        [ResponseType(typeof(List<SecurityLoginsLogPoco>))]
+        public IHttpActionResult GetAllSecurityLoginLog()
+        {
+            List<SecurityLoginsLogPoco> securityLoginLog = _logic.GetAll();
+            if (securityLoginLog == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLoginLog);
+        }
+
+        [HttpPost]
+        [Route("loginLog")]
+        public IHttpActionResult SaveSecurityLoginLog(SecurityLoginsLogPoco[] securityLoginLog)
+        {
+            if (securityLoginLog == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(securityLoginLog);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("loginLog")]
+        public IHttpActionResult UpdateSecurityLoginLog(SecurityLoginsLogPoco[] securityLoginLog)
+        {
+            if (securityLoginLog == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(securityLoginLog);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("loginLog")]
+        public IHttpActionResult DeleteSecurityLoginLog(SecurityLoginsLogPoco[] securityLoginLog)
+        {
+            if (securityLoginLog == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(securityLoginLog);
+            return Ok();
         }
     }
 }
