a07a86b9086e42ddc04e80bc147b7a529f1e33bf
Hiren Dave <hdave3180@outlook.com>
2018-04-20 21:38:16 -0400

    SystemLanguageCodeController Done

--- CareerCloud.WebAPI/Controllers/SystemLanguageCodeController.cs
+++ CareerCloud.WebAPI/Controllers/SystemLanguageCodeController.cs
@@ -8,6 +8,7 @@ using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
 using CareerCloud.DataAccessLayer;
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -15,11 +16,75 @@ namespace CareerCloud.WebAPI.Controllers
     public class SystemLanguageCodeController : ApiController
     {
         private SystemLanguageCodeLogic _logic;
-        private IDataRepository<SystemLanguageCodePoco> repository;
-
+       
         public SystemLanguageCodeController()
         {
-            _logic = new SystemLanguageCodeLogic(repository);
+            _logic = new SystemLanguageCodeLogic(new EFGenericRepository<SystemLanguageCodePoco>(false));
+        }
+
+
+        [HttpGet]
+        [Route("languageCode/{systemLanguageCodeId}")]
+        [ResponseType(typeof(SystemLanguageCodePoco))]
+        public IHttpActionResult GetLanguageCode(string systemLanguageCodeId)
+        {
+            SystemLanguageCodePoco languageCode = _logic.GetSingle(systemLanguageCodeId);
+            if (languageCode == null)
+            {
+                return NotFound();
+            }
+            return Ok(languageCode);
+        }
+
+
+        [HttpGet]
+        [Route("languageCode")]
+        [ResponseType(typeof(List<SystemLanguageCodePoco>))]
+        public IHttpActionResult GetAllLanguageCode()
+        {
+            List<SystemLanguageCodePoco> languageCode = _logic.GetAll();
+            if (languageCode == null)
+            {
+                return NotFound();
+            }
+            return Ok(languageCode);
+        }
+
+        [HttpPost]
+        [Route("languageCode")]
+        public IHttpActionResult SaveLanguageCode(SystemLanguageCodePoco[] languageCode)
+        {
+            if (languageCode == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(languageCode);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("languageCode")]
+        public IHttpActionResult UpdateLanguageCode(SystemLanguageCodePoco[] languageCode)
+        {
+            if (languageCode == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(languageCode);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("languageCode")]
+        public IHttpActionResult DeleteLanguageCode(SystemLanguageCodePoco[] languageCode)
+        {
+            if (languageCode == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(languageCode);
+            return Ok();
         }
     }
 }
