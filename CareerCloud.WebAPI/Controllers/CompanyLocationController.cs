73934dab7e84276f4032c8cd5cb4e06de443c8af
Hiren Dave <hdave3180@outlook.com>
2018-04-20 20:31:50 -0400

    CompanyLocationController Done

--- CareerCloud.WebAPI/Controllers/CompanyLocationController.cs
+++ CareerCloud.WebAPI/Controllers/CompanyLocationController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,71 @@ namespace CareerCloud.WebAPI.Controllers
         public CompanyLocationController()
         {
             _logic = new CompanyLocationLogic(new EFGenericRepository<CompanyLocationPoco>(false));
+        }
+
+
+        [HttpGet]
+        [Route("location/{companyLocationId}")]
+        [ResponseType(typeof(CompanyLocationPoco))]
+        public IHttpActionResult GetCompanyLocation(Guid companyLocationId)
+        {
+            CompanyLocationPoco companyLocation = _logic.Get(companyLocationId);
+            if (companyLocation == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyLocation);
+        }
+
+
+        [HttpGet]
+        [Route("Location")]
+        [ResponseType(typeof(List<CompanyLocationPoco>))]
+        public IHttpActionResult GetAllCompanyLocation()
+        {
+            List<CompanyLocationPoco> companyLocation = _logic.GetAll();
+            if (companyLocation == null)
+            {
+                return NotFound();
+            }
+            return Ok(companyLocation);
+        }
+
+        [HttpPost]
+        [Route("Location")]
+        public IHttpActionResult SaveCompanyLocation(CompanyLocationPoco[] companyLocation)
+        {
+            if (companyLocation == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(companyLocation);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("Location")]
+        public IHttpActionResult UpdateCompanyLocation(CompanyLocationPoco[] companyLocation)
+        {
+            if (companyLocation == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(companyLocation);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("Location")]
+        public IHttpActionResult DeleteCompanyLocation(CompanyLocationPoco[] companyLocation)
+        {
+            if (companyLocation == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(companyLocation);
+            return Ok();
         }
     }
 }
