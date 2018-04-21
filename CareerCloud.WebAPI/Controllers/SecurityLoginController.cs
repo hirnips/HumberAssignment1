f8d2569710bc6f352d90aa0709ff96fce034a524
Hiren Dave <hdave3180@outlook.com>
2018-04-20 20:47:43 -0400

    SecurityLoginController Done

--- CareerCloud.WebAPI/Controllers/SecurityLoginController.cs
+++ CareerCloud.WebAPI/Controllers/SecurityLoginController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,70 @@ namespace CareerCloud.WebAPI.Controllers
         public SecurityLoginController()
         {
             _logic = new SecurityLoginLogic(new EFGenericRepository<SecurityLoginPoco>(false));
+        }
+
+        [HttpGet]
+        [Route("login/{securityLoginId}")]
+        [ResponseType(typeof(SecurityLoginPoco))]
+        public IHttpActionResult GetSecurityLogin(Guid securityLoginId)
+        {
+            SecurityLoginPoco securityLogin = _logic.Get(securityLoginId);
+            if (securityLogin == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLogin);
+        }
+
+
+        [HttpGet]
+        [Route("login")]
+        [ResponseType(typeof(List<SecurityLoginPoco>))]
+        public IHttpActionResult GetAllSecurityLogin()
+        {
+            List<SecurityLoginPoco> securityLogin = _logic.GetAll();
+            if (securityLogin == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLogin);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IHttpActionResult SaveSecurityLogin(SecurityLoginPoco[] securityLogin)
+        {
+            if (securityLogin == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(securityLogin);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("login")]
+        public IHttpActionResult UpdateSecurityLogin(SecurityLoginPoco[] securityLogin)
+        {
+            if (securityLogin == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(securityLogin);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("login")]
+        public IHttpActionResult DeleteSecurityLogin(SecurityLoginPoco[] securityLogin)
+        {
+            if (securityLogin == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(securityLogin);
+            return Ok();
         }
     }
 }
