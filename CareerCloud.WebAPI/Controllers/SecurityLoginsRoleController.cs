75998deece0be1248ac6ba23fd4341417e0cde71
Hiren Dave <hdave3180@outlook.com>
2018-04-20 20:56:59 -0400

    SecurityLoginRoleController Done

--- CareerCloud.WebAPI/Controllers/SecurityLoginsRoleController.cs
+++ CareerCloud.WebAPI/Controllers/SecurityLoginsRoleController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -19,6 +19,70 @@ namespace CareerCloud.WebAPI.Controllers
         public SecurityLoginsRoleController()
         {
             _logic = new SecurityLoginsRoleLogic(new EFGenericRepository<SecurityLoginsRolePoco>(false));
+        }
+
+        [HttpGet]
+        [Route("loginRole/{securityLoginRoleId}")]
+        [ResponseType(typeof(SecurityLoginsRolePoco))]
+        public IHttpActionResult GetSecurityLoginRole(Guid securityLoginRoleId)
+        {
+            SecurityLoginsRolePoco securityLoginRole = _logic.Get(securityLoginRoleId);
+            if (securityLoginRole == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLoginRole);
+        }
+
+
+        [HttpGet]
+        [Route("loginRole")]
+        [ResponseType(typeof(List<SecurityLoginsRolePoco>))]
+        public IHttpActionResult GetAllSecurityLoginRole()
+        {
+            List<SecurityLoginsRolePoco> securityLoginRole = _logic.GetAll();
+            if (securityLoginRole == null)
+            {
+                return NotFound();
+            }
+            return Ok(securityLoginRole);
+        }
+
+        [HttpPost]
+        [Route("loginRole")]
+        public IHttpActionResult SaveSecurityLoginRole(SecurityLoginsRolePoco[] securityLoginRole)
+        {
+            if (securityLoginRole == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(securityLoginRole);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("loginRole")]
+        public IHttpActionResult UpdateSecurityLoginRole(SecurityLoginsRolePoco[] securityLoginRole)
+        {
+            if (securityLoginRole == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(securityLoginRole);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("loginRole")]
+        public IHttpActionResult DeleteSecurityLoginRole(SecurityLoginsRolePoco[] securityLoginRole)
+        {
+            if (securityLoginRole == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(securityLoginRole);
+            return Ok();
         }
     }
 }
