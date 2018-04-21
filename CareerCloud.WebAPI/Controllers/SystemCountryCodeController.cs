897c1acb233c4e7c1752beba717acfb5fb99b80c
Hiren Dave <hdave3180@outlook.com>
2018-04-20 21:29:19 -0400

    SystemCountryCodeController Done

--- CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
+++ CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
@@ -8,7 +8,7 @@ using CareerCloud.BusinessLogicLayer;
 using CareerCloud.Pocos;
 using CareerCloud.EntityFrameworkDataAccess;
 using CareerCloud.DataAccessLayer;
-
+using System.Web.Http.Description;
 
 namespace CareerCloud.WebAPI.Controllers
 {
@@ -16,11 +16,74 @@ namespace CareerCloud.WebAPI.Controllers
     public class SystemCountryCodeController : ApiController
     {
         private SystemCountryCodeLogic _logic;
-        private IDataRepository<SystemCountryCodePoco> repository;
-
+        
         public SystemCountryCodeController()
-        {             
-            _logic = new SystemCountryCodeLogic(repository);
+        {            
+            _logic = new SystemCountryCodeLogic(new EFGenericRepository<SystemCountryCodePoco>(false));
+        }
+
+        [HttpGet]
+        [Route("code/{systemCountryCodeId}")]
+        [ResponseType(typeof(SystemCountryCodePoco))]
+        public IHttpActionResult GetCountryCode(string systemCountryCodeId)
+        {
+            SystemCountryCodePoco countryCode = _logic.GetSingle(systemCountryCodeId);
+            if (countryCode == null)
+            {
+                return NotFound();
+            }
+            return Ok(countryCode);
+        }
+
+
+        [HttpGet]
+        [Route("code")]
+        [ResponseType(typeof(List<SystemCountryCodePoco>))]
+        public IHttpActionResult GetAllCountryCode()
+        {
+            List<SystemCountryCodePoco> countryCode = _logic.GetAll();
+            if (countryCode == null)
+            {
+                return NotFound();
+            }
+            return Ok(countryCode);
+        }
+
+        [HttpPost]
+        [Route("code")]
+        public IHttpActionResult SaveCountryCode(SystemCountryCodePoco[] countryCode)
+        {
+            if (countryCode == null)
+            {
+                return NotFound();
+            }
+            _logic.Add(countryCode);
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("code")]
+        public IHttpActionResult UpdateCountryCode(SystemCountryCodePoco[] countryCode)
+        {
+            if (countryCode == null)
+            {
+                return NotFound();
+            }
+            _logic.Update(countryCode);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("code")]
+        public IHttpActionResult DeleteCountryCode(SystemCountryCodePoco[] countryCode)
+        {
+            if (countryCode == null)
+            {
+                return NotFound();
+            }
+            _logic.Delete(countryCode);
+            return Ok();
         }
     }
 }
